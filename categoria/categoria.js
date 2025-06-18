7471de4f43b3d86e86138bd22c39711e79842d68
Arthur Venâncio <arthurmvenancio@gmail.com>
2025-06-17 21:43:08 -0300

    lista de itens feita

--- categoria/categoria.js
+++ categoria/categoria.js
@@ -1,33 +1,52 @@
 
 async function trazerObjetoDB(chave) {
     const db = await fetch("../db/db.json")
-    .then(response => response.json())
+        .then(response => response.json())
 
     const link = db[chave];
     const response = await fetch(link);
     const texto = await response.text()
 
-    const linhas = texto.trim().split("\n").map(linha => linha.split(","))
 
-    const [cabecalho, ...dados] = linhas
+    const resultadoPapa = Papa.parse(texto, {
+        header: true,
+        skipEmptyLines: true,
+    });
+
+    const linhas = resultadoPapa.data;
 
     const resultado = Object.fromEntries(
-        dados.map(colunas => {
-            const chaveExterna = colunas[0]
-            const objetoInterno = Object.fromEntries(
-                cabecalho.slice(1).map((chave, i) => [chave, colunas[i + 1]])
-            )
-            return [chaveExterna, objetoInterno]
+        linhas.map(linha => {
+            const { [Object.keys(linha)[0]]: chaveExterna, ...resto } = linha;
+            return [chaveExterna, resto];
         })
-    )
+    );
 
     return resultado
 }
 
+async function preencherItens() {
+    const categoria = sessionStorage.getItem("categoria");
+    const itens = await trazerObjetoDB(categoria);
+    const container = document.getElementById("itens-container");
+    container.innerHTML = "";
+    console.log(itens);
+    Object.keys(itens).forEach(item => {
+        const cartao = document.createElement("div");
+        cartao.className = "item-card";
+        cartao.innerHTML = `
+        <span>${item}</span>
+        <span>$${itens[item].Price}</span>`
+        container.appendChild(cartao);
+    })
+}
+
 document.addEventListener("DOMContentLoaded", async () => {
-    
+
     const categoria = sessionStorage.getItem("categoria")
     const titulo = document.getElementById("categoria-titulo")
     titulo.innerHTML = `${categoria}`
+
+    await preencherItens();
 
 })
